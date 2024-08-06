594cb3398586f73d59cda78a0bbb5460f7f3cbd4
HydraCat <roan.h.gibbons@gmail.com>
2024-08-07 00:41:08 +0100

    Added duplication check, overrode GUI elements, started on scrollbar

--- traitacquirer/traitacquirerModSystem.cs
+++ traitacquirer/traitacquirerModSystem.cs
@@ -10,6 +10,9 @@ using Newtonsoft.Json.Linq;
 using Vintagestory.API.Common.Entities;
 using System.Linq;
 using System;
+using System.Data;
+using System.Xml.Linq;
+using System.Text;
 
 namespace traitacquirer
 {
@@ -25,8 +28,13 @@ namespace traitacquirer
         public List<CharacterClass> characterClasses = new List<CharacterClass>();
         public Dictionary<string, Trait> TraitsByCode = new Dictionary<string, Trait>();
         public Dictionary<string, CharacterClass> characterClassesByCode = new Dictionary<string, CharacterClass>();
-        //public CharacterSystem characterSystem = new();
+        GuiDialogCharacterBase charDlg;
 
+        GuiElementRichtext logtextElem;
+        ElementBounds clippingBounds;// = ElementBounds.Fixed(0, 0, innerWidth - 20 - 1, 200 - 1).FixedUnder(commmandsBounds, spacing - 10);
+        ElementBounds scrollbarBounds;// = clippingBounds.CopyOffsetedSibling(clippingBounds.fixedWidth + 6, -1).WithFixedWidth(20).FixedGrow(0, 2);
+        ElementBounds commmandsBounds;// = ElementBounds.Fixed(0, 30, innerWidth, 30);
+        int spacing = 5;
         public override void Start(ICoreAPI api)
         {
             this.api = api;
@@ -36,14 +44,123 @@ namespace traitacquirer
         public override void StartServerSide(ICoreServerAPI api)
         {
             this.sapi = api;
-            //traits = api.Assets.Get("config/traits.json").ToObject<List<Trait>>();
             loadCharacterClasses();
             api.Event.RegisterEventBusListener(OnAcquireTrait, 0.5, "traitAcquisition");
+            //api.RegisterCommand();
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
             this.capi = api;
+            loadCharacterClasses();
+            charDlg = api.Gui.LoadedGuis.Find(dlg => dlg is GuiDialogCharacterBase) as GuiDialogCharacterBase;
+            charDlg.RenderTabHandlers.Add(composeTraitsTab);
+        }
+
+        private void composeTraitsTab(GuiComposer compo)
+        {
+            charDlg.SingleComposer.GetTextArea("");
+            this.clippingBounds = ElementBounds.Fixed(0, 25, 385 - 5 - 1, 200 - 1);//.FixedUnder(commmandsBounds, spacing - 10);
+            compo.BeginClip(clippingBounds);
+            compo.AddRichtext(getClassTraitText(), CairoFont.WhiteDetailText().WithLineHeightMultiplier(1.15), ElementBounds.Fixed(0, 25, 385, 200), "text");
+            //compo.AddDynamicText(getClassTraitText(), CairoFont.WhiteDetailText().WithLineHeightMultiplier(1.15), ElementBounds.Fixed(0, 25, 385, 200), "text");
+            this.logtextElem = compo.GetRichtext("text");
+            //logtextElem.Bounds.CalcWorldBounds();
+            //double innerWidth = logtextElem.Bounds.absInnerWidth;
+            //this.commmandsBounds = ElementBounds.Fixed(0, 30, innerWidth, 30);
+            this.scrollbarBounds = clippingBounds.CopyOffsetedSibling(clippingBounds.fixedWidth + 6, -1).WithFixedWidth(20).FixedGrow(0, 2);
+
+            compo.AddVerticalScrollbar(OnNewScrollbarvalue, this.scrollbarBounds, "scrollbar");
+            compo.EndClip();
+        }
+        private void OnNewScrollbarvalue(float value)
+        {
+            //GuiElementDynamicText logtextElem = compo.GetDynamicText("text");
+            logtextElem.Bounds.fixedY = 3 - value;
+            logtextElem.Bounds.CalcWorldBounds();
+        }
+
+        string getClassTraitText()
+        {
+            string charClass = capi.World.Player.Entity.WatchedAttributes.GetString("characterClass");
+            CharacterClass chclass = characterClasses.FirstOrDefault(c => c.Code == charClass);
+
+            StringBuilder fulldesc = new StringBuilder();
+            StringBuilder attributes = new StringBuilder();
+
+            var chartraits = chclass.Traits.Select(code => TraitsByCode[code]).OrderBy(trait => (int)trait.Type);
+
+            foreach (var trait in chartraits)
+            {
+                attributes.Clear();
+                foreach (var val in trait.Attributes)
+                {
+                    if (attributes.Length > 0) attributes.Append(", ");
+                    attributes.Append(Lang.Get(string.Format(GlobalConstants.DefaultCultureInfo, "charattribute-{0}-{1}", val.Key, val.Value)));
+                }
+
+                if (attributes.Length > 0)
+                {
+                    fulldesc.AppendLine(Lang.Get("traitwithattributes", Lang.Get("trait-" + trait.Code), attributes));
+                }
+                else
+                {
+                    string desc = Lang.GetIfExists("traitdesc-" + trait.Code);
+                    if (desc != null)
+                    {
+                        fulldesc.AppendLine(Lang.Get("traitwithattributes", Lang.Get("trait-" + trait.Code), desc));
+                    }
+                    else
+                    {
+                        fulldesc.AppendLine(Lang.Get("trait-" + trait.Code));
+                    }
+
+
+                }
+            }
+
+            if (chclass.Traits.Length == 0)
+            {
+                fulldesc.AppendLine(Lang.Get("No positive or negative traits"));
+            }
+
+            fulldesc.AppendLine(Lang.Get("Extra Traits: "));
+
+            string[] extraTraits = capi.World.Player.Entity.WatchedAttributes.GetStringArray("extraTraits");
+
+            var extratraits = extraTraits.OrderBy(code => (int)TraitsByCode[code].Type);
+            //var extratraits = chclass.Traits.Select(code => TraitsByCode[code]).OrderBy(trait => (int)trait.Type);
+
+            foreach (var code in extratraits)
+            {
+                attributes.Clear();
+                foreach (var val in TraitsByCode[code].Attributes)
+                {
+                    if (attributes.Length > 0) attributes.Append(", ");
+                    attributes.Append(Lang.Get(string.Format(GlobalConstants.DefaultCultureInfo, "charattribute-{0}-{1}", val.Key, val.Value)));
+                }
+
+                if (attributes.Length > 0)
+                {
+                    fulldesc.AppendLine(Lang.Get("traitwithattributes", Lang.Get("trait-" + code), attributes));
+                }
+                else
+                {
+                    string desc = Lang.GetIfExists("traitdesc-" + code);
+                    if (desc != null)
+                    {
+                        fulldesc.AppendLine(Lang.Get("traitwithattributes", Lang.Get("trait-" + code), desc));
+                    }
+                    else
+                    {
+                        fulldesc.AppendLine(Lang.Get("trait-" + code));
+                    }
+
+
+                }
+            }
+
+            return fulldesc.ToString();
         }
 
         private void OnAcquireTrait(string eventName, ref EnumHandling handling, IAttribute data)
@@ -77,22 +194,23 @@ namespace traitacquirer
                 {
                     newExtraTraits.AddRange(extraTraits);
                 }
-                newExtraTraits.Add(traitName);
-                plr.Entity.WatchedAttributes.SetStringArray("extraTraits", newExtraTraits.ToArray());
-                applyTraitAttributes(plr.Entity);
-                plr.SendIngameError("Trait Acquire Attempt", Lang.Get("Trait Acquire Attempt"));
-                //var chapters = (itemslot.Itemstack.Attributes["chapterIds"] as IntArrayAttribute).value;
-                //discovery = new LoreDiscovery()
-                //{
-                //    Code = discCode,
-                //    ChapterIds = new List<int>(chapters)
-                //};
+                if (!newExtraTraits.Contains(traitName))
+                {
+                    newExtraTraits.Add(traitName);
+                    plr.Entity.WatchedAttributes.SetStringArray("extraTraits", newExtraTraits.ToArray());
+                    applyTraitAttributes(plr.Entity);
+                    plr.SendIngameError("Trait Acquired", Lang.Get("Trait Acquired"));
+                }
+                else
+                {
+                    plr.SendIngameError("Trait Already Acquired", Lang.Get("Trait Already Acquired"));
+                }
+                
             }
 
             if (trait == null)
             {
                 plr.SendIngameError("Trait is Null", Lang.Get("Trait is Null"));
-                //plr.SendIngameError("alreadydiscovered", Lang.Get("Nothing new in these pages"));
                 return;
             }
 
@@ -104,7 +222,7 @@ namespace traitacquirer
             //DiscoverLore(discovery, plr, itemslot);
         }
 
-        public void applyTraitAttributes(EntityPlayer eplr)
+        public void applyTraitAttributes(EntityPlayer eplr) //Taken from SurvivalMod Character.cs, CharacterSystem class where it is a private method
         {
             string classcode = eplr.WatchedAttributes.GetString("characterClass");
             CharacterClass charclass = characterClasses.FirstOrDefault(c => c.Code == classcode);
@@ -123,10 +241,7 @@ namespace traitacquirer
                 }
             }
 
-
-
             // Then apply
-            //string[] addedTrait = []; 
             string[] extraTraits = eplr.WatchedAttributes.GetStringArray("extraTraits");
             var allTraits = extraTraits == null ? charclass.Traits : charclass.Traits.Concat(extraTraits);
 
@@ -147,11 +262,11 @@ namespace traitacquirer
 
             eplr.GetBehavior<EntityBehaviorHealth>()?.MarkDirty();
         }
-        private void loadCharacterClasses()
+        private void loadCharacterClasses() //Taken from SurvivalMod Character.cs, CharacterSystem class where it is a private method
         {
             //onLoadedUniversal();
-            traits = api.Assets.Get("config/traits.json").ToObject<List<Trait>>();
-            characterClasses = api.Assets.Get("config/characterclasses.json").ToObject<List<CharacterClass>>();
+            this.traits = api.Assets.Get("config/traits.json").ToObject<List<Trait>>();
+            this.characterClasses = api.Assets.Get("config/characterclasses.json").ToObject<List<CharacterClass>>();
 
             foreach (var trait in traits)
             {
